f4d0ab85e37352bb4884a515a6050027d113ff3c
Wael El Echi <echi.wael@gmail.com>
2022-01-17 13:21:26 +0100

    add: Blockchain validation

--- MyChain.js
+++ MyChain.js
@@ -1,13 +1,12 @@
 const SHA256 = require("crypto-js/sha256");
 
 class Block {
-  Constructor(data, previousHash) {
+  constructor(data, previousHash) {
     this.data = data;
     this.timestamp = Date.now();
     this.previousHash = previousHash;
     this.hash = this.getHash();
   }
-
   getHash() {
     return SHA256(
       this.previousHash + this.timestamp + JSON.stringify(this.data)
@@ -25,6 +24,19 @@ class Blockchain {
   createBlock(data) {
     this.blockchain.push(new Block(data, this.getLastBlock().hash));
   }
+  isBlockchainValid() {
+    for (let i = 1; i < this.blockchain.length; i++) {
+      let currentBlock = this.blockchain[i];
+      let previousBlock = this.blockchain[i - 1];
+      if (
+        currentBlock.previousHash !== previousBlock.hash ||
+        currentBlock.hash !== currentBlock.getHash()
+      ) {
+        return false;
+      }
+    }
+    return true;
+  }
 }
 
 myChain = new Blockchain();
@@ -37,3 +49,8 @@ console.log(myChain);
 myChain.createBlock("another set of transaction data");
 console.log("\n-----\nAdded another block");
 console.log(myChain);
+
+console.log("Is the chain valid: " + myChain.isBlockchainValid());
+myChain.blockchain[1].data = "changed set of transactions";
+console.log("Modified transaction data in a block");
+console.log("Is the chain valid: " + myChain.isBlockchainValid());
